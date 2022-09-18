c060acc6d9aec5751c0dbe136a8a6670ed6198bf
tgprogrammer <goyaltania728@gmail.com>
2022-09-18 16:13:10 -0400

    Updations on the project

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modal = document.querySelectorAll('.show-modal');
+const openModal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for(let i = 0; i < modal.length; i ++){
+    modal[i].addEventListener('click', function () {
+        openModal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+}
+
+closeModal.addEventListener('click', function() {
+    openModal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
